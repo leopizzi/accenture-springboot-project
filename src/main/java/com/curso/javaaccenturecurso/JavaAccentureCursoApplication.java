2c9d039bcdae090562ae7ab6454656bcfe9955b4
Pizzi <leonardo.n.pizzi@accenture.com>
2019-05-21 14:57:02 -0300

    Bug Fixes

--- src/main/java/com/curso/javaaccenturecurso/JavaAccentureCursoApplication.java
+++ src/main/java/com/curso/javaaccenturecurso/JavaAccentureCursoApplication.java
@@ -21,9 +21,7 @@ public class JavaAccentureCursoApplication {
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(
-						RequestHandlerSelectors
-						.basePackage("com.curso.javaaccenturecurso.controller"))
+				.apis(RequestHandlerSelectors.basePackage("com.curso.javaaccenturecurso.controller"))
 				.build();
 	}
 }
